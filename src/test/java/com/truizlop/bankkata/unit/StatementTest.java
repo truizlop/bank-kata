32cacc6ec14f41807bcb658a6fa53fe17caabb22
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-04-20 16:50:35 +0200

    Statement prints transactions in the right order

--- src/test/java/com/truizlop/bankkata/unit/StatementTest.java
+++ src/test/java/com/truizlop/bankkata/unit/StatementTest.java
@@ -4,12 +4,14 @@ import com.truizlop.bankkata.Statement;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.PrintStream;
 
 import static com.truizlop.bankkata.builder.TransactionBuilder.aTransaction;
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -31,22 +33,18 @@ public class StatementTest {
     }
 
     @Test
-    public void shouldPrintDeposit(){
+    public void shouldPrintInTheRightOrder(){
         statement.addLineContaining(
                 aTransaction().withAmount(1000).onDate("01/02/2003").build(), 1000
         );
-        statement.printTo(printer);
-
-        verify(printer).println("01/02/2003 | 1000.00 | - | 1000.00");
-    }
-
-    @Test
-    public void shouldPrintWithdrawals(){
         statement.addLineContaining(
-                aTransaction().withAmount(-500).onDate("01/02/2003").build(), -500
+                aTransaction().withAmount(-300).onDate("02/02/2003").build(), 700
         );
         statement.printTo(printer);
 
-        verify(printer).println("01/02/2003 | - | 500.00 | -500.00");
+        InOrder inOrder = inOrder(printer);
+        inOrder.verify(printer).println("date | deposit | withdrawal | balance");
+        inOrder.verify(printer).println("02/02/2003 | - | 300.00 | 700.00");
+        inOrder.verify(printer).println("01/02/2003 | 1000.00 | - | 1000.00");
     }
 }
